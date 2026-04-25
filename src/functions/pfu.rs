294230b3d0a9df81dae8a8ec673a8e3656cfb699
Nicolas Beaudouin <pro@nicolasbeaudouin.com>
2026-04-25 20:09:53 +0200

    🐛 Fix capital gain not properly defined with fees
    * capital_gain= net_proceeds - (portfolio_cost_basis * net_proceeds / portfolio_total_value)
    ** with net_proceeds = gross_sale_price - selling_fees

--- src/functions/pfu.rs
+++ src/functions/pfu.rs
@@ -4,6 +4,10 @@ use rust_decimal_macros::dec;
 
 /* Calculate the french "plus-value" to fill the form 2086
 
+capital_gain = net_proceeds - (portfolio_cost_basis * net_proceeds / portfolio_total_value)
+where net_proceeds = gross_sale_price - selling_fees
+
+In French :
 plus_value =  prix_cession - (acquisition_pf_net * prix_cession / valeur_pf )
 
 The transaction must be taxable, otherwise it will panic !
@@ -23,7 +27,7 @@ pub fn calculate_tax_gains(tx: &Transaction, portfolio: &Portfolio, cost_basis: 
             let pf_total_value = portfolio.pf_total_value;
             let sell_price: Decimal = Decimal::from(*amount) * from.price_eur;
             let fee = to.fee.unwrap_or(dec!(0)) * to.price_eur + from.fee.unwrap_or(dec!(0)) * from.price_eur;
-            return _calculate_tax(sell_price, fee,cost_basis, pf_total_value);
+            return _calculate_tax(sell_price, fee, cost_basis, pf_total_value);
         }
         _ => dec!(0),
     }
@@ -35,14 +39,15 @@ pub fn _calculate_tax(
     cost_basis: &GlobalCostBasis,
     pf_total_value: Decimal,
 ) -> Decimal {
-    return (sell_price - total_fee) - calculate_weigted_price(sell_price, cost_basis.pf_cost_basis, pf_total_value);
+    let net_proceeds = sell_price - total_fee;
+    return net_proceeds - calculate_weigted_price(net_proceeds, cost_basis.pf_cost_basis, pf_total_value);
 }
 
-/* (acquisition_pf_net * prix_cession / valeur_pf ) */
+/* portfolio_cost_basis * net_proceeds / portfolio_total_value */
 pub fn calculate_weigted_price(
-    sell_price: Decimal,
+    net_proceeds: Decimal,
     current_cost_basis: Decimal,
     pf_total_value: Decimal,
 ) -> Decimal {
-    return current_cost_basis * sell_price / pf_total_value;
+    return current_cost_basis * net_proceeds / pf_total_value;
 }
